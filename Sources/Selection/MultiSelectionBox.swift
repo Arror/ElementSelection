3570bce64c1e983182e82a48e9261b91181203e7
Arror <hallo.maqiang@gmail.com>
2018-01-21 18:08:12 +0800

    Add MultiSelectionBox

---
+++ Sources/Selection/MultiSelectionBox.swift
@@ -0,0 +1,37 @@
+//
+//  MultiSelectionBox.swift
+//  ElementSelection
+//
+//  Created by Arror on 2018/1/21.
+//
+
+import Foundation
+
+open class MultiSelectionBox<MES: MultiElementSelection>: AbstractMultiSelection<MES.Container, MES.MultiSelectionContainer> where MES.Container.Element: Identifiable, MES.Container.Element.Identifier: Hashable {
+    
+    private let inner: MES
+    
+    public init(inner: MES) {
+        self.inner = inner
+    }
+    
+    open override var elements: [MES.Container.Element] {
+        return self.inner.elements
+    }
+    
+    open override func isSelected(of element: MES.Container.Element) -> Bool {
+        return self.inner.isSelected(of: element)
+    }
+    
+    open override func set(selected isSelected: Bool, for element: MES.Container.Element) {
+        self.inner.set(selected: isSelected, for: element)
+    }
+    
+    open override func updateSelectionIfElementContainerAvailable() {
+        self.inner.clear()
+    }
+    
+    open override func clear() {
+        self.inner.clear()
+    }
+}
